24582a90784c07a1b9e0bcf7f958417c61591f5c
Tony Chandra <tonytonychan89@gmail.com>
2023-08-28 14:38:05 +0700

    update generator

--- server/src/services/generator/generate-4d-array.ts
+++ server/src/services/generator/generate-4d-array.ts
@@ -1,20 +1,27 @@
-import { Bet } from '../../models/bet';
-import generate_number_array from '../../utils/generate-array-number';
-import remove_2d_kembar from '../../utils/remove-2d-kembar';
+import { Bet } from '../../models/bet'
+import {
+  one_digit_generate_number_array,
+  two_digit_generate_number_array,
+} from '../../utils/generate-array-number'
+import remove_2d_kembar from '../../utils/remove-2d-kembar'
 
 interface Generate4DArrrayParams {
-  pasaran: string;
-  match_query: string;
+  pasaran: string
+  match_query: string
 }
 
 const generate_4d_array = async ({
   pasaran,
   match_query,
 }: Generate4DArrrayParams) => {
-  const least_bet_2d_: string[] = [];
-  const array_4d_to_check = [];
-  const all_bet_data: any[] = [];
-  const number_array = generate_number_array(); // ['1', '2', '3', '4]
+  const least_bet_2d: string[] = []
+  const array_3d_to_check: any[] = []
+  const array_4d_to_check: any[] = []
+  const all_bet_data_2d: any[] = []
+  const all_bet_data_3d: any[] = []
+  const all_bet_data_4d: any[] = []
+  const one_digit_number_array = one_digit_generate_number_array()
+  const two_digit_number_array = two_digit_generate_number_array()
 
   const data_pasaran = await Bet.aggregate([
     {
@@ -29,61 +36,86 @@ const generate_4d_array = async ({
         periode: { $max: '$periode' },
       },
     },
-  ]);
+  ])
 
   for (const data of data_pasaran) {
-    const bettingan = await Bet.find(
-      { website: data._id, periode: data.periode },
+    const bettingan2D = await Bet.find(
+      { website: data._id, periode: data.periode, bet_type: '2D' },
       { _id: 0, bet_number: 1 }
-    );
+    )
+    const bettingan3D = await Bet.find(
+      { website: data._id, periode: data.periode, bet_type: '3D' },
+      { _id: 0, bet_number: 1 }
+    )
 
-    const array_all_bet = bettingan.map(item => item.bet_number);
+    const bettingan4D = await Bet.find(
+      { website: data._id, periode: data.periode, bet_type: '4D' },
+      { _id: 0, bet_number: 1 }
+    )
 
-    all_bet_data.push(...array_all_bet);
+    const array_all_bet_2d = bettingan2D.map(item => item.bet_number)
+    const array_all_bet_3d = bettingan3D.map(item => item.bet_number)
+    const array_all_bet_4d = bettingan4D.map(item => item.bet_number)
+
+    all_bet_data_2d.push(...array_all_bet_2d)
+    all_bet_data_3d.push(...array_all_bet_3d)
+    all_bet_data_4d.push(...array_all_bet_4d)
   }
 
-  number_array.forEach(number => {
-    const matches = all_bet_data.filter(item => item == number).length;
+  two_digit_number_array.forEach(number => {
+    const matches = all_bet_data_2d.filter(item => item == number).length
 
     if (matches < +match_query) {
-      least_bet_2d_.push(number);
+      least_bet_2d.push(number)
     }
-  });
+  })
 
-  if (!least_bet_2d_.length)
-    throw new Error(`Could not find least number for 2D`);
+  if (!least_bet_2d.length)
+    throw new Error(`Could not find least number for 2D`)
 
-  const bet_2d_array = remove_2d_kembar(least_bet_2d_);
-
-  const non_identical_number_array = remove_2d_kembar(number_array);
+  const bet_2d_array = remove_2d_kembar(least_bet_2d)
 
   if (bet_2d_array.length < 3)
-    throw new Error('Not enough data generated for 2D');
+    throw new Error('Not enough data generated for 2D')
 
   for (const item of bet_2d_array) {
-    for (const item2 of non_identical_number_array) {
-      array_4d_to_check.push(item2 + item);
-    }
-  }
-  const generated_4d = [];
-
-  for (let i = 0; i < array_4d_to_check.length; i++) {
-    const current_item = array_4d_to_check[i];
-    let is_duplicate = false;
-
-    for (let j = 0; j < array_4d_to_check.length; j++) {
-      if (i !== j && current_item === array_4d_to_check[j]) {
-        is_duplicate = true;
-        break;
-      }
-    }
-
-    if (!is_duplicate) {
-      generated_4d.push(current_item);
+    for (const item2 of one_digit_number_array) {
+      array_3d_to_check.push(item2 + item)
     }
   }
 
-  return generated_4d;
-};
+  const generated_3d: any[] = []
 
-export default generate_4d_array;
+  array_3d_to_check.forEach(number => {
+    const matches = all_bet_data_3d.filter(item => item == number).length
+
+    if (matches <= 1) {
+      generated_3d.push(number)
+    }
+  })
+
+  if (generated_3d.length < 3)
+    throw new Error('Not enough data generated for 3D')
+
+  const generated_4d: any[] = []
+
+  for (const item of generated_3d) {
+    for (const item2 of one_digit_number_array) {
+      array_4d_to_check.push(item2 + item)
+    }
+  }
+
+  array_4d_to_check.forEach(number => {
+    const matches = all_bet_data_4d.filter(item => item == number).length
+
+    if (matches == 0) {
+      generated_4d.push(number)
+    }
+  })
+
+  if (!generated_4d.length) throw new Error('Not enough data generated for 4D')
+
+  return generated_4d
+}
+
+export default generate_4d_array
--- server/src/utils/generate-array-number.ts
+++ server/src/utils/generate-array-number.ts
@@ -1,11 +1,19 @@
-function generate_number_array() {
-  const number_array = [];
+export function two_digit_generate_number_array() {
+  const number_array = []
 
   for (let number = 0; number < 100; number++) {
-    number_array.push(number < 10 ? `0${number}` : `${number}`);
+    number_array.push(number < 10 ? `0${number}` : `${number}`)
   }
 
-  return number_array;
+  return number_array
 }
 
-export default generate_number_array;
+export function one_digit_generate_number_array() {
+  const number_array = []
+
+  for (let number = 0; number < 10; number++) {
+    number_array.push(`${number}`)
+  }
+
+  return number_array
+}
