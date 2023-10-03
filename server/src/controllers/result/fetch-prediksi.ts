1a7d7395a0cd7ee700ce09903ea55dd1e7c8c016
Tony Chandra <tonytonychan89@gmail.com>
2023-10-03 17:03:19 +0700

    update to async

--- server/src/controllers/result/fetch-prediksi.ts
+++ server/src/controllers/result/fetch-prediksi.ts
@@ -8,7 +8,6 @@ import { randomChalk } from 'ody-utils'
 import { website_list } from '../../constants/website-list'
 import Result from '../../models/result'
 
-
 const fetch_prediksi_controller = async (req: Request, res: Response) => {
   const pasaran_query = req.query.pasaran_query as string
   const show_kembar = req.query.show_kembar as string
@@ -28,44 +27,81 @@ const fetch_prediksi_controller = async (req: Request, res: Response) => {
 
   const angka_prediksi = get_random_data(array_to_check)
 
-  for (const website of filtered_website_list) {
-    const { phpsessid: PHPSESSID } = await IDN.login({
-      username: website.username,
-      password: website.password,
-      pin: website.pin,
-      base_url: website.baseURL,
-    })
+  // for (const website of filtered_website_list) {
+  //   const { phpsessid: PHPSESSID } = await IDN.login({
+  //     username: website.username,
+  //     password: website.password,
+  //     pin: website.pin,
+  //     base_url: website.baseURL,
+  //   })
 
-    const periode = await IDN.get_last_periode({
-      kode_pasaran: website.pasaran,
-      PHPSESSID,
-      base_URL: website.baseURL,
-    })
+  //   const periode = await IDN.get_last_periode({
+  //     kode_pasaran: website.pasaran,
+  //     PHPSESSID,
+  //     base_URL: website.baseURL,
+  //   })
 
-    for (const arr_of_three_4d_digit of angka_prediksi) {
-      const { hasil, detail } = await get_prediksi_result({
-        angka_prediksi: arr_of_three_4d_digit.map(num => num.toString()),
-        periode,
+  //   for (const arr_of_three_4d_digit of angka_prediksi) {
+  //     const { hasil, detail } = await get_prediksi_result({
+  //       angka_prediksi: arr_of_three_4d_digit.map(num => num.toString()),
+  //       periode,
+  //       kode_pasaran: website.pasaran,
+  //       PHPSESSID,
+  //       baseURL: website.baseURL,
+  //     })
+
+  //     detail['website'] = website.website
+
+  //     const new_prediksi_data = Result.build({
+  //       angka_keluar: arr_of_three_4d_digit.map(num => num.toString()),
+  //       hasil: return_plain_num(hasil),
+  //       pasaran: pasaran_query,
+  //       total_omset: detail['Total Omset'] ? +detail['Total Omset'] : 0,
+  //       detail: detail,
+  //     })
+
+  //     await new_prediksi_data.save()
+  //   }
+
+  //   randomChalk('Berhasil menyimpan data prediksi')
+  // }
+
+  await Promise.all(
+    filtered_website_list.map(async website => {
+      const { phpsessid: PHPSESSID } = await IDN.login({
+        username: website.username,
+        password: website.password,
+        pin: website.pin,
+        base_url: website.baseURL,
+      })
+      const periode = await IDN.get_last_periode({
         kode_pasaran: website.pasaran,
         PHPSESSID,
-        baseURL: website.baseURL,
+        base_URL: website.baseURL,
       })
+      for (const arr_of_three_4d_digit of angka_prediksi) {
+        const { hasil, detail } = await get_prediksi_result({
+          angka_prediksi: arr_of_three_4d_digit.map(num => num.toString()),
+          periode,
+          kode_pasaran: website.pasaran,
+          PHPSESSID,
+          baseURL: website.baseURL,
+        })
 
-      detail['website'] = website.website
+        detail['website'] = website.website
 
-      const new_prediksi_data = Result.build({
-        angka_keluar: arr_of_three_4d_digit.map(num => num.toString()),
-        hasil: return_plain_num(hasil),
-        pasaran: pasaran_query,
-        total_omset: detail['Total Omset'] ? +detail['Total Omset'] : 0,
-        detail: detail,
-      })
-
-      await new_prediksi_data.save()
-    }
-
-    randomChalk('Berhasil menyimpan data prediksi')
-  }
+        const new_prediksi_data = Result.build({
+          angka_keluar: arr_of_three_4d_digit.map(num => num.toString()),
+          hasil: return_plain_num(hasil),
+          pasaran: pasaran_query,
+          total_omset: detail['Total Omset'] ? +detail['Total Omset'] : 0,
+          detail: detail,
+        })
+        await new_prediksi_data.save()
+      }
+      randomChalk('Berhasil menyimpan data prediksi')
+    })
+  )
 
   res
     .status(200)
